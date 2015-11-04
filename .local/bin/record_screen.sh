7d5ede546e9221029de11838cdf1f420c6dd2958
Mark Stillwell <mark@stillwell.me>
2015-11-04 14:56:14 +0000

    autocommit

--- .local/bin/record_screen.sh
+++ .local/bin/record_screen.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 OUTFILE=$1
 [ -z "$OUTFILE" ] && OUTFILE=screengrab.avi
-#ffmpeg -f x11grab -i ${DISPLAY} -c:v mpeg4 -q:v 1 -f alsa -ac 2 -i default -itsoffset 00:00:00.5 -loglevel error $OUTFILE
 ffmpeg -f alsa -i hw:1 -f x11grab -s 1920x1080 -i ${DISPLAY} -c:v mpeg4 -q:v 1 $OUTFILE
